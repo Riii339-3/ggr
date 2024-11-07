d1a438f9058bae99f726c196df65aade63ab30e0
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2024-11-07 23:29:36 +0900

    追加: 検索ボックスを追加する #14

--- scripts/ggr.js
+++ scripts/ggr.js
@@ -1,27 +1,27 @@
-const ggr = () => {
-  const hash = location.hash.substring(1);
-  const encodedHash = hash.replace(/&/g, "%26").replace(/#/, "%23");
-  if (!encodedHash) {
-    document.getElementById("tips").style.display = "unset";
-    return;
-  }
-  const urls = {};
-  Object.entries({
-    "Google": "search?q=",
-    "Bing": "search?q=",
-    "DuckDuckGo": "?q=",
-    "Yahoo": "search?p=",
-    "Brave": "search?q=",
-    "Ecosia": "search?q=",
-  }).forEach(([engine, query]) => {
-    urls[engine] = query + encodedHash;
-  });
-  const search = document.getElementById("search");
-  search.innerHTML += decodeURI(hash.replace("+", " ")).replaceAll("<", "&lt;").replaceAll(">", "&gt;");
-  search.style.display = "unset";
+const encodeHash = (hash) => hash.replace(/&/g, "%26").replace(/#/, "%23").replace(/ /g, "+");
+const applyHash = (hash) => {
+  hash = encodeHash(hash);
+  const urls = {
+    "Google": ["https://www.google.com/", "search?q="],
+    "Bing": ["https://www.bing.com/", "search?q="],
+    "DuckDuckGo": ["https://duckduckgo.com/", "?q="],
+    "Yahoo": ["https://search.yahoo.com/", "search?p="],
+    "Brave": ["https://search.brave.com/", "search?q="],
+    "Ecosia": ["https://www.ecosia.org/", "search?q="],
+  };
   Array.from(document.body.querySelectorAll("li > a")).forEach((element) => {
     const href = element.attributes.getNamedItem("href");
-    href.value += urls[element.textContent];
+    href.value = hash ? urls[element.textContent].join("") + hash : urls[element.textContent][0];
   });
 };
-window.addEventListener("hashchange", () => location.reload());
+const ggr = () => {
+  const hash = location.hash.substring(1);
+  if (!hash) return;
+  applyHash(hash);
+  const search = document.getElementById("search");
+  search.value = decodeURI(hash.replace("+", " ")).replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+};
+document.getElementById("search").addEventListener("input", (event) => {
+  location.hash = event.target.value ? "#" + event.target.value.replaceAll(" ", "+") : "";
+  applyHash(event.target.value);
+});
